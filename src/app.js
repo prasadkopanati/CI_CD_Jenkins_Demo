4c6ca660406c3d826528175044b5b58eeaab423d
Prasad Kopanati <PrasadKopanati@gmail.com>
2022-01-19 16:08:55 +0530

    Updated tests and functions in the API

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-   res.send("Hello to IIT-J");
+   res.send("Hello to IIT-J and family");
 });
 
 module.exports = app;
