15d6cbd82a831fb7ba9a9e18d77232c37a725707
Prasad Kopanati <PrasadKopanati@gmail.com>
2022-01-22 14:53:49 +0530

    Updated tests

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-   res.send("Hello to IIT-J and family");
+   res.send("Hello and Namaste to IIT-J and family");
 });
 
 module.exports = app;
